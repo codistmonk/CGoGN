cd6f8b4883edd3b198b4d51a2a2b353b48adeb0c
codistmonk <codistmonk@gmail.com>
2012-11-17 16:39:46 +0100

    Fixed a defect in debug.h that was causing a compilation issue in release mode.

--- include/Utils/debug.h
+++ include/Utils/debug.h
@@ -1,11 +1,20 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+#include <sstream>
+
 #ifndef NDEBUG
 #define DEBUG_OUT std::cout << "(" __FILE__ << ':' << __LINE__ << ") "
 #define DEBUG_GL { GLenum error = glGetError(); while (error) { DEBUG_OUT << gluErrorString(error) << std::endl; error = glGetError(); } }
 #else
-#define DEBUG_OUT //
+namespace Debug
+{
+
+static std::stringstream ignore;
+
+} // namespace Debug
+
+#define DEBUG_OUT Debug::ignore
 #define DEBUG_GL
 #endif
 

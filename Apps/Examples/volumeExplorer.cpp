e7ed75f69038c829519fddda6cf7373cbfe82539
codistmonk <codistmonk@gmail.com>
2012-12-05 16:03:54 +0100

    Updated testRasterizeTriangle() in volumeExplorer.

--- Apps/Examples/volumeExplorer.cpp
+++ Apps/Examples/volumeExplorer.cpp
@@ -942,19 +942,21 @@ static void testRasterizeTriangle()
 
 	GLint const viewportX = 0;
 	GLint const viewportY = 0;
-	GLint const viewportWidth = 4;
+	GLint const viewportWidth = 20;
 	GLint const viewportHeight = viewportWidth;
 	GLint const right = viewportWidth - 1;
 	GLint const top = viewportHeight - 1;
-	int errorCount = 0;
+	int totalErrorCount = 0;
+	QImage image(viewportWidth, viewportHeight, QImage::Format_ARGB32);
+	image.fill(QColor(0, 0, 0));
 
 	DEBUG_OUT << "viewport: " << viewportX << ' ' << viewportY << ' ' << viewportWidth << ' ' << viewportHeight << std::endl;
 
 	RasterizationSpans spans(viewportHeight);
 	FragmentBuffer fragmentBuffer(viewportHeight * viewportWidth);
 
-	int const rgba1 = 0xFFFF00FF;
-	int const rgba2 = 0xFF00FFFF;
+	int const rgba1 = 0xFFFF0000;
+	int const rgba2 = 0x7F00FF00;
 
 	std::vector< int > expectedRowSummary0;
 	expectedRowSummary0.push_back(0);
@@ -975,12 +977,29 @@ static void testRasterizeTriangle()
 	expectedRowSummary3.push_back(rgba2);
 	expectedRowSummary3.push_back(0);
 
-	for (int i = 0; i <= viewportWidth; ++i)
+	std::vector< int > expectedRowSummary4;
+	expectedRowSummary4.push_back(0);
+	expectedRowSummary4.push_back(rgba2);
+	expectedRowSummary4.push_back(rgba1);
+	expectedRowSummary4.push_back(0);
+
+	for (int i = 0; i < 2 * viewportWidth; ++i)
 	{
-		glm::vec4 const v1(i, 0.0f, 0.0f, 1.0f);
-		glm::vec4 const v2(right - i, top, 0.0f, 1.0f);
-		glm::vec4 const v3(0.0f, top - i, 0.0f, 1.0f);
-		glm::vec4 const v4(0.0f, i, 0.0f, 1.0f);
+		glm::vec4 v1(i, 0.0f, 0.0f, 1.0f);
+		glm::vec4 v2(right - i, top, 0.0f, 1.0f);
+		glm::vec4 v3(0.0f, top - i, 0.0f, 1.0f);
+		glm::vec4 v4(right, i, 0.0f, 1.0f);
+
+		if (viewportWidth <= i)
+		{
+			int const j = i - viewportWidth;
+			v1 = glm::vec4(right, j, 0.0f, 1.0f);
+			v2 = glm::vec4(0.0f, top - j, 0.0f, 1.0f);
+			v3 = glm::vec4(j, 0.0f, 0.0f, 1.0f);
+			v4 = glm::vec4(right - j, top, 0.0f, 1.0f);
+		}
+
+		int currentErrorCount = 0;
 
 		clearStacks(fragmentBuffer);
 
@@ -999,7 +1018,7 @@ static void testRasterizeTriangle()
 				{
 					if (fragments.size() != 1)
 					{
-						++errorCount;
+						++currentErrorCount;
 
 						DEBUG_OUT << "i: " << i << " y: " << y << " x: " << x << " badFragmentCount: " << fragments.size() << std::endl;
 					}
@@ -1026,18 +1045,36 @@ static void testRasterizeTriangle()
 			}
 
 			if (!(expectedRowSummary0 == actualRowSummary || expectedRowSummary1 == actualRowSummary ||
-					expectedRowSummary2 == actualRowSummary || expectedRowSummary3 == actualRowSummary))
+					expectedRowSummary2 == actualRowSummary || expectedRowSummary3 == actualRowSummary ||
+					expectedRowSummary4 == actualRowSummary))
 			{
-				++errorCount;
+				++currentErrorCount;
 
 				using namespace Debug;
 
 				DEBUG_OUT << "i: " << i << " y: " << y << " badRowSummary: " << actualRowSummary << std::endl;
 			}
 		}
+
+		totalErrorCount += currentErrorCount;
+
+		if (0 < currentErrorCount)
+		{
+			sortAndBlend(fragmentBuffer, image);
+
+			std::stringstream fileName;
+			fileName << "testRasterizeTriangle_" << i << ".png";
+
+			QFile file(fileName.str().c_str());
+
+			if (file.open(QIODevice::WriteOnly))
+			{
+				image.save(&file);
+			}
+		}
 	}
 
-	if (errorCount == 0)
+	if (totalErrorCount == 0)
 	{
 		DEBUG_OUT << "rasterizeTriangle() seems OK" << std::endl;
 	}
@@ -1045,6 +1082,8 @@ static void testRasterizeTriangle()
 	{
 		DEBUG_OUT << "rasterizeTriangle() is defective" << std::endl;
 	}
+
+	assert(totalErrorCount == 0);
 }
 
 int main(int argc, char **argv)
